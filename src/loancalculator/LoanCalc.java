77df0a5c2fbdbea04c30a9b2568a568b41708825
Franz Bowman <namwoblinus@gmail.com>
2016-05-26 13:53:09 -0500

    Initial Build

---
+++ src/loancalculator/LoanCalc.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loancalculator;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author bowmanFS
+ */
+public class LoanCalc {
+    
+    private BigDecimal principal;
+    private BigDecimal remainingPrincipal;
+    private double rate;
+    private int numPeriods;
+    private int periodNumber;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        promptUser();
+        calculate();
+    }
+
+    private static void promptUser() {
+        //TODO To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    private static void calculate() {
+        //TODO To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+}
