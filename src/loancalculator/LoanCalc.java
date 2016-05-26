55f6f33f30a9caabfd1038a1c2c8b544b0fa1b88
Franz Bowman <namwoblinus@gmail.com>
2016-05-26 14:16:58 -0500

    Get input from user for principal, rate and numPeriods.

--- src/loancalculator/LoanCalc.java
+++ src/loancalculator/LoanCalc.java
@@ -6,6 +6,7 @@
 package loancalculator;
 
 import java.math.BigDecimal;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,11 +14,11 @@ import java.math.BigDecimal;
  */
 public class LoanCalc {
     
-    private BigDecimal principal;
-    private BigDecimal remainingPrincipal;
-    private double rate;
-    private int numPeriods;
-    private int periodNumber;
+    private static BigDecimal principal;
+    private static BigDecimal remainingPrincipal;
+    private static double rate;
+    private static int numPeriods;
+    private static int periodNumber;
 
     /**
      * @param args the command line arguments
@@ -28,7 +29,14 @@ public class LoanCalc {
     }
 
     private static void promptUser() {
-        //TODO To change body of generated methods, choose Tools | Templates.
+        //get input from user
+        // Get the starting amount
+        String strPrincipal = JOptionPane.showInputDialog("Enter the starting amount");
+        principal = new BigDecimal(strPrincipal);
+        // get the rate per period
+        rate = Double.parseDouble(JOptionPane.showInputDialog("Enter rate per period"));
+        // get the number of periods
+        numPeriods = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of periods"));
         
     }
 
